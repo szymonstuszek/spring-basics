d3833a1062865a66c3339785c01ae80e6ab02cd4
szymon <szymonstuszek@gmail.com>
2022-03-10 06:10:49 +0100

    Config and bean

---
+++ src/main/java/pl/mentoring/devs/firstspringproject/model/Course.java
@@ -0,0 +1,7 @@
+package pl.mentoring.devs.firstspringproject.model;
+
+public class Course {
+    public void displayLessons() {
+        System.out.println("No lessons yet included in this course.");
+    }
+}
